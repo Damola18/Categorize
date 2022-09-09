2df1cf541aec64d45336a61152700be104dab390
Damola Olutoke <olutokeezekiel18@gmail.com>
2022-09-09 03:20:24 +0100

    First Commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,103 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WellaMvc.DatabaseContext;
+using WellaMvc.Models;
+
+namespace WellaMvc.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly AppDbContext _context;
+        public UserController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var user = _context.UserDataTable.ToList();
+            return View(user);
+        }
+
+        public IActionResult AddUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddUser(UserData user)
+        {
+            if (ModelState.IsValid)
+            {
+                await _context.UserDataTable.AddAsync(user);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> DetailsAsync(int ID)
+        {   
+            var user = await _context.UserDataTable.FirstOrDefaultAsync(x => x.ID == ID);
+
+            return View(user);
+        }
+
+        public async Task<IActionResult> EditAsync(int ID)
+        {   
+            var user = await _context.UserDataTable.FirstOrDefaultAsync(x => x.ID == ID);
+
+            return View(user);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> EditAsync(UserData model)
+        {   
+            var user = await _context.UserDataTable.FirstOrDefaultAsync(x => x.ID == model.ID);
+
+            if (!string.IsNullOrEmpty(model.FirstName)) user.FirstName = model.FirstName; 
+            if (!string.IsNullOrEmpty(model.LastName)) user.LastName = model.LastName;
+            if (!string.IsNullOrEmpty(model.Email)) user.Email = model.Email;
+            if (!string.IsNullOrEmpty(model.PhoneNumber)) user.PhoneNumber = model.PhoneNumber;
+            if (!string.IsNullOrEmpty(model.Location)) user.Location = model.Location;
+
+            _context.UserDataTable.Update(user);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> Delete(int ID){
+            {   
+                var user = await _context.UserDataTable.FirstOrDefaultAsync(x => x.ID == ID);
+
+                return View(user);
+            }
+
+            var userFromDb = _context.UserDataTable.Find(ID);
+        
+            return View(userFromDb);
+        }
+
+        // DELETE USER
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteUser(int ID){
+            var item = _context.UserDataTable.Find(ID);
+            // var item = _context.UserDataTable.FirstOrDefaultAsync(x => x.ID == ID);
+            if( item != null ){
+                return View(item);
+               
+            }
+            _context.UserDataTable.Remove(item);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+           
+        }
+    }
+}
---
+++ Migrations/20220908074300_DataToDatabase.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WellaMvc.Migrations
+{
+    public partial class DataToDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
